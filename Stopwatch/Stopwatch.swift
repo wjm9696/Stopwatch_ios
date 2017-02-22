6b74d5fb490153daaa1c6064293a545a52b9c7cd
Jimmy <jimmy@airbears2-10-142-118-76.airbears2.1918.berkeley.edu>
2017-02-21 22:53:56 -0800

    finished

--- Stopwatch/Stopwatch.swift
+++ Stopwatch/Stopwatch.swift
@@ -7,3 +7,48 @@
 //
 
 import Foundation
+
+class Stopwatch{
+    var on = false
+    var date = Date()
+    init() {
+        
+
+    }
+    
+    func start(){
+        on = true
+        date = Date()
+
+    }
+    
+    func stop(){
+        on = false
+        
+
+    }
+    
+    func getTimeString() -> String{
+        var timeString: String
+        if(on){
+            let timeLapse = -date.timeIntervalSinceNow
+            let seconds: Int = Int(timeLapse)
+            let mm: Int = Int((timeLapse - Double(seconds)) * 100)
+            let minutes: Int = seconds / 60
+            let realSeconds = seconds % 60
+            timeString = String(format: "%02d:%02d.%02d", minutes, realSeconds, mm)
+            
+        }else{
+            timeString = "00:00.00"
+        }
+        return timeString
+
+    }
+    
+    
+    
+
+    
+    
+
+}
--- Stopwatch/ViewController.swift
+++ Stopwatch/ViewController.swift
@@ -9,17 +9,72 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var label: UILabel!
 
+    @IBOutlet weak var startButton: UIButton!
+    var watchController = Stopwatch()
+    var pause = false
+    var stop = true
+    var on = false
+    var timer: Timer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    
+    @IBAction func startTimer(_ sender: UIButton) {
+        if(on){
+            return;
+        }
+        else if( pause ){
+            resetLabel()
+            startButton.setTitle("Start",for: .normal);
+            stop = true
+            pause = false
+            
+        }else if(stop){
+                        watchController.start()
+            timer = Timer.scheduledTimer(timeInterval: 0.01, target: self,
+                                             selector: #selector(ViewController.updateLabel(_:)),
+                                             userInfo: nil,
+                                             repeats: true)
+            
+            stop = false
+            on = true
+        }
+    }
+    
+    @IBAction func stopTimer(_ sender: UIButton) {
+        if(on){
+            watchController.stop()
+            startButton.setTitle("Reset",for: .normal);
+            if let time = timer{
+                time.invalidate();
+            }
+            on = false
+            pause = true
+        }
+        
+        
+    }
+    
+    func updateLabel(_ timer:Timer){
+        label.text = watchController.getTimeString()
+    }
+    
+    func resetLabel(){
+        label.text = "00:00,00"
+    }
+    
+   
 
 }
 
